22d9ad0e459c7439fad9a3893759718b1a180065
OlegAndreev <andreev008@mail.ru>
2023-07-15 01:10:56 +0300

    add task2

---
+++ task2/task2.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите 3 числа. Число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3 числа. Число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3 числа. Число 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number2 > max)
+{
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+
+Console.WriteLine($"Максимальное из чисел равно {max}");
